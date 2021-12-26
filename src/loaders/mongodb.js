d314810d494a700e417f1c0039fc73d7bffa0bfa
gkandemi <gokhan@gkandemir.com>
2021-12-26 14:16:45 +0300

    MongoDB instance added

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -7,8 +7,12 @@ db.once("open", () => {
 });
 
 const connectDB = async () => {
-  const { DB_HOST, DB_PORT, DB_NAME } = process.env;
-  await Mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+  const { DB_HOST, DB_PORT, DB_NAME, DB_CONNECT_URL } = process.env;
+  let CONNECT_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+  if (process.env.NODE_ENV === "development") {
+    CONNECT_URL = DB_CONNECT_URL;
+  }
+  await Mongoose.connect(CONNECT_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
