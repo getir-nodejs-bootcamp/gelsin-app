3e1c6b9574e1f159d729f10f0ffe6c68057ca688
gkandemi <gokhan@gkandemir.com>
2021-12-26 14:18:36 +0300

    MongoDB instance added

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -9,7 +9,7 @@ db.once("open", () => {
 const connectDB = async () => {
   const { DB_HOST, DB_PORT, DB_NAME, DB_CONNECT_URL } = process.env;
   let CONNECT_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === "production") {
     CONNECT_URL = DB_CONNECT_URL;
   }
   await Mongoose.connect(CONNECT_URL, {
